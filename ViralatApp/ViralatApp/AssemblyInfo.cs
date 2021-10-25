f3811f78f8b6bb5d2881d28b40ecdbc5e0d73d06
Eladio Junior Rodriguez Rodriguez <eladiojuniorrodriguez@gmail.com>
2021-10-24 21:22:32 -0400

    feat: DetailView

--- ViralatApp/ViralatApp/AssemblyInfo.cs
+++ ViralatApp/ViralatApp/AssemblyInfo.cs
@@ -4,5 +4,6 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Roboto-Medium.ttf", Alias = "RobotoMedium")]
 [assembly: ExportFont("Roboto-Regular.ttf", Alias = "RobotoRegular")]
+[assembly: ExportFont("Roboto-Bold.ttf", Alias = "RobotoBold")]
 [assembly: ExportFont("PaytoneOne-Regular.ttf", Alias = "PaytoneOneRegular")]
 [assembly: ExportFont("materialdesignicons.ttf", Alias = "MaterialIcons")]
